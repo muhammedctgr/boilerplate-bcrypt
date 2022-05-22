e26f406a9aae5a47938c210863d1acd291aa8207
Gbenga <73699892+muhammedctgr@users.noreply.github.com>
2022-05-22 12:33:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 
 //END_ASYNC
